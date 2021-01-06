005d26adaed4548982000f41b901d3664a0047a8
Damien Trouillet <contact@damien-trouillet.fr>
2021-01-06 10:49:46 +0100

    first commit

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-explore-container',
@@ -7,9 +9,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
+  data: any;
 
-  constructor() { }
+  constructor(private barcodeScanner: BarcodeScanner) { }
 
   ngOnInit() {}
 
+  async openBrowser() {
+    // On iOS, for example, open the URL in SFSafariViewController (the in-app browser)
+    await Plugins.Browser.open({ url: 'https://google.com' });
+  }
+
+  scan() {
+    this.data = null;
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData.text);
+      this.data = barcodeData;
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
+import {BarcodeScanResult} from '@ionic-native/barcode-scanner';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  data: BarcodeScanResult;
+  constructor(private barcodeScanner: BarcodeScanner, private alertController: AlertController) { }
 
-  constructor() {}
+  async presentAlert(title: string, body: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: body,
+      buttons: ['OK']
+    });
 
+    await alert.present();
+  }
+
+  scan() {
+    this.data = null;
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData.text);
+      this.data = barcodeData;
+      if(this.data.format !== 'QR_CODE'){
+        this.presentAlert('Erreur', 'Aucun QRCode trouvé');
+        return;
+      }
+      this.presentAlert('Résultat', this.data.text);
+    }).catch(err => {
+      this.presentAlert('Erreur', err.message);
+    });
+  }
 }
